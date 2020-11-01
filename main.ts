4ea59f8a0aa80f78a299f6058d01e858edeb8e23
Cyril <github@ituilerie.com>
2020-11-01 10:44:05 +0100

    Simplification, remplacement par des boucles

--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
 function Eteindre () {
-    led.unplot(0, 0)
-    basic.pause(150)
-    led.unplot(1, 1)
-    basic.pause(150)
-    led.unplot(2, 2)
-    basic.pause(150)
-    led.unplot(3, 3)
-    basic.pause(150)
-    led.unplot(4, 4)
-    basic.pause(150)
+    for (let index = 0; index <= 4; index++) {
+        led.unplot(index, index)
+        basic.pause(150)
+    }
     led.unplot(3, 4)
     led.unplot(4, 3)
     basic.pause(150)
@@ -16,36 +10,38 @@ function Eteindre () {
     led.unplot(4, 2)
 }
 function Allumer () {
-    led.plot(0, 0)
-    basic.pause(150)
-    led.plot(1, 1)
-    basic.pause(150)
-    led.plot(2, 2)
-    basic.pause(150)
-    led.plot(3, 3)
-    basic.pause(150)
-    led.plot(4, 4)
-    basic.pause(150)
+    for (let index = 0; index <= 4; index++) {
+        led.plot(index, index)
+        basic.pause(150)
+    }
     led.plot(4, 3)
     led.plot(3, 4)
     basic.pause(150)
     led.plot(4, 2)
     led.plot(2, 4)
 }
+let index = 0
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        for (let index = 0; index < 3; index++) {
-            Allumer()
-            Eteindre()
+        basic.showString("Salut Cyril")
+        basic.pause(120)
+        for (let index = 0; index <= 10; index++) {
+            basic.pause(120)
+            basic.showNumber(10 - index)
         }
     } else if (input.buttonIsPressed(Button.B)) {
-        for (let index = 0; index < 10; index++) {
+        for (let index2 = 0; index2 < 10; index2++) {
             basic.showIcon(IconNames.Heart)
             basic.pause(150)
             basic.clearScreen()
             basic.pause(150)
         }
+    } else if (input.buttonIsPressed(Button.AB)) {
+        for (let index2 = 0; index2 < 3; index2++) {
+            Allumer()
+            Eteindre()
+        }
     } else {
-        led.unplot(2, 2)
+        basic.clearScreen()
     }
 })
